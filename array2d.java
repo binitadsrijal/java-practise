b5ed2332b5e6a98e7e1d498b65867c9aa5edca3f
Binita rijal <rijalbinita01@gmail.com>
2023-07-30 14:21:35 +0545

    transpose

--- array2d.java
+++ array2d.java
@@ -48,7 +48,7 @@ public class array2d {
 */
 
 //matrix multiplication-------------------------------------------------------------*
-
+/* 
 public class array2d{
       static void printii(int b[][]) {
         for (int i = 0; i < b.length; i++) {
@@ -89,5 +89,54 @@ public class array2d{
         System.out.println("array2");
         printii(arr2);
 mult(arr,r1,c1,arr2,r2,c2);
+    }
+}
+*/
+
+//transpose of matrix----------------------------------------------------------------------*
+
+public class array2d{
+    //printing
+       static void printii(int b[][]) {
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
+                System.out.print(b[i][j]+" ");
+            }
+            System.out.println();
+        }
+    } 
+
+    // static int[][] transpose(int arr[][],int r,int c){  //using extra array
+    //     int ans[][]=new int[r][c];
+    //     for(int i=0;i<r;i++){
+    //         for(int j=0;j<c;j++){
+    //             ans[i][j]=arr[j][i];
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+    //changing same matrix------------------------------------*
+
+       static void transpose(int arr[][],int r,int c){
+        for(int i=0;i<r;i++){
+            for(int j=i;j<c;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
+        }
+    }
+    public static void main(String[]args){
+        int arr[][]={{1,2,3},{3,4,5},{6,7,8}};
+        int r=3;
+        int c=3;
+        System.out.println("original matrix");
+        printii(arr);
+//int ans[][]=transpose(arr, r, c);
+transpose(arr, r, c);
+System.out.println("transpose");
+printii(arr);
+//printii(ans);
     }
 }
